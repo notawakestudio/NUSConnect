eb9fe67ef25cdfad6af49252aba2508b0a2579d3
tlylt <liu_yongliang@hotmail.com>
2021-05-03 23:30:32 +0800

    Add testing tool

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+    setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+    moduleNameMapper: {
+        "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    },
+};
---
+++ tests/index.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+
+describe("Home", () => {
+    it("renders without crashing", () => {
+        render(<Home />);
+        expect(
+            screen.getByRole("heading", { level: 1 })
+        ).toBeInTheDocument();
+    });
+});
