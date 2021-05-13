e82f28e0d711a3bea3e30f859e6d0eb33acb45a2
tlylt <liu_yongliang@hotmail.com>
2021-05-13 13:21:30 +0800

    Add some components

---
+++ components/common/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = (): JSX.Element => {
+  return (
+    <footer className="my-2 p-2 mx-auto shadow-inner max-w-max font-bold bg-gray-600 text-gray-100">
+      <a href="" target="_blank" rel="noopener noreferrer">
+        Powered by <span className="text-gray-200">NotAwakeStudio</span>
+      </a>
+    </footer>
+  )
+}
+
+export default Footer
