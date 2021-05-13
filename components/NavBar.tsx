96c3231ae9a79aa38a1d08e67a4e467a65a1fb0b
ong6 <junxiongong2@gmail.com>
2021-05-13 22:31:04 +0800

    Update quiz names and started working on forum

---
+++ components/NavBar.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+import styles from './NavBar.module.scss'
+import Link from 'next/link'
+
+const NavBar = (props) => {
+    
+
+}
---
+++ components/forum/Post.tsx
@@ -0,0 +1,7 @@
+import Link from 'next/link'
+
+const Post = (): JSX.Element => {
+  return <Link href="/post"></Link>
+}
+
+export default Post
