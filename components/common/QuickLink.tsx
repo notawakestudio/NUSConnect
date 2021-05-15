4bb414d244be4d613a7ff640071e06aeb2256071
tlylt <liu_yongliang@hotmail.com>
2021-05-15 20:56:17 +0800

    Fix quiz logic

--- components/common/QuickLink.tsx
+++ components/common/QuickLink.tsx
@@ -7,7 +7,7 @@ const QuickLink = (): JSX.Element => {
       <div>
         <button
           type="button"
-          className="px-4 py-2 w-28 bg-gray-600 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+          className="px-2 py-2 w-24 bg-gray-600 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
           id="options-menu"
           onClick={() => setCollapse(!collapse)}
         >
