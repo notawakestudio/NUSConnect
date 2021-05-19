f9262beb132b6d44e9830c0dd421b6eaf8a66e4d
tlylt <liu_yongliang@hotmail.com>
2021-05-19 09:42:07 +0800

    Add editor demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import Team from '../components/landingPage/Team'
 
 export default function Home(): JSX.Element {
   return (
-  <>
+    <>
       <Head>
         <title>NUS Connect | NotAwakeStudio</title>
         <meta name="description" content="NUS Connect" />
@@ -66,7 +66,6 @@ export default function Home(): JSX.Element {
         <Team />
         <Pricing />
       </div>
-
       <Footer />
     </>
   )
