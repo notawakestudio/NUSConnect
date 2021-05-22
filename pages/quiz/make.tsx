13a93148d933ff9dc96b37b9c01626772b4d06d4
tlylt <liu_yongliang@hotmail.com>
2021-05-22 10:04:21 +0800

    Minor update to default content

--- pages/quiz/make.tsx
+++ pages/quiz/make.tsx
@@ -5,7 +5,7 @@ import matter from 'gray-matter'
 import { createQuestion } from '../../components/quiz/QuizAPI'
 
 const defaultQuestionFormat =
-  '---\ntype: MCQ\nmodules: \n- CS2030\n- CS2030S\ncorrect_answers: \n- "Collection of related classes and interfaces which are bundled together"\nincorrect_answers: \n- "We should declare fields as private as much as possible"\n- "We should not throw exceptions that reveal internal implementation of a class as much as possible"\n- "We should avoid using accessors and mutators (also known as getters and setters) to private fields as much as possible"\n- "We should use polymorphism so that each class is responsible for handling its own behavior as much as possible"\n- "None of the others"\n---\nquestion: "What is a package in java?" '
+  '---\ntype: MCQ\nmodules: \n- CS2030\n- CS2030S\ncorrect_answers: \n- "Collection of related classes and interfaces which are bundled together"\nincorrect_answers: \n- "We should declare fields as private as much as possible"\n- "We should not throw exceptions that reveal internal implementation of a class as much as possible"\n- "We should avoid using accessors and mutators (also known as getters and setters) to private fields as much as possible"\n- "We should use polymorphism so that each class is responsible for handling its own behavior as much as possible"\n- "None of the others"\n---\nWhat is a package in java?'
 const handleSubmit = (value): void => {
   console.log(matter(value))
   createQuestion(matter(value))
