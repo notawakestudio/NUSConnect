648c99a00a4e94d5d03614f6d83cbb5f97fc8294
tlylt <liu_yongliang@hotmail.com>
2021-06-27 00:04:13 +0800

    More tests

--- cypress/integration/quiz_spec.ts
+++
@@ -1,9 +0,0 @@
-describe('Quiz page', function () {
-  before(() => {
-    cy.log(`Visiting quiz page`)
-    cy.visit('/quiz')
-  })
-  it.skip('verifies UI', function () {
-    cy.contains('All Quizzes')
-  })
-})
