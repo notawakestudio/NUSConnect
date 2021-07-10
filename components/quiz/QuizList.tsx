7458ab471144baf6e521152607d76577e2307122
tlylt <liu_yongliang@hotmail.com>
2021-07-10 09:15:10 +0800

    Fix sorting

--- components/quiz/QuizList.tsx
+++ components/quiz/QuizList.tsx
@@ -4,19 +4,28 @@ import { AiOutlineSearch } from 'react-icons/ai'
 import { IoMdAddCircleOutline } from 'react-icons/io'
 import Search, { Design } from '../common/Search'
 import QuizItemCard from './QuizItemCard'
+import { Radio, RadioGroup, Stack } from '@chakra-ui/react'
 import { Quiz } from './types'
 const QuizList = ({ quizzes }: { quizzes: Quiz[] }): JSX.Element => {
   const [query, setQuery] = useState('')
   const [filteredQuizzes, setFilteredQuizzes] = useState<Quiz[]>([])
+  const [sortBy, setSortBy] = useState('title')
   useEffect(() => {
-    if (query.trim() === '') {
-      setFilteredQuizzes(quizzes)
+    if (sortBy === 'title') {
+      setFilteredQuizzes(() => [...quizzes].sort((a, b) => a.title.localeCompare(b.title)))
     } else {
-      setFilteredQuizzes(
-        quizzes.filter((quiz) => quiz.title.toLowerCase().includes(query.toLowerCase()))
+      setFilteredQuizzes(() =>
+        [...quizzes].filter((quiz) => quiz.title.toLowerCase().includes(query.toLowerCase()))
       )
     }
-  }, [query, quizzes])
+    if (query.trim() === '') {
+      setFilteredQuizzes((filteredQuizzes) => filteredQuizzes)
+    } else {
+      setFilteredQuizzes((filteredQuizzes) =>
+        filteredQuizzes.filter((quiz) => quiz.title.toLowerCase().includes(query.toLowerCase()))
+      )
+    }
+  }, [query, quizzes, sortBy])
   return (
     <div className="flex flex-col w-full">
       <div className="px-4 md:px-6 pt-20">
@@ -25,6 +34,13 @@ const QuizList = ({ quizzes }: { quizzes: Quiz[] }): JSX.Element => {
           <div className="sm:flex items-center w-auto p-1 dark:text-gray-200">
             <AiOutlineSearch size={25} className="mr-1 hidden md:flex" />
             <Search design={Design.square} query={query} setQuery={setQuery} />
+            <RadioGroup onChange={setSortBy} value={sortBy}>
+              <Stack direction="row">
+                <span className="m-2">Sort by:</span>
+                <Radio value="title">Title</Radio>
+                <Radio value="week">Week</Radio>
+              </Stack>
+            </RadioGroup>
           </div>
           <Link href={'/quiz/make-question'}>
             <span className="shadow-md p-2 cursor-pointer bg-white hover:bg-indigo-200 dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-gray-500 flex flex-row items-center w-auto h-auto">
