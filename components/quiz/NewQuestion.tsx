f313dcb893ea21dccbd7c56dc0b7a7c878e34e75
ong6 <junxiongong2@gmail.com>
2021-07-23 21:50:18 +0800

    test fix

--- components/quiz/NewQuestion.tsx
+++ components/quiz/NewQuestion.tsx
@@ -142,7 +142,7 @@ export default function NewQuestion(): JSX.Element {
                   <div className="w-full">
                     <Field
                       as="textarea"
-                      name="Question"
+                      name="question"
                       rows={5}
                       className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full px-4 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-100 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                       placeholder="Question"></Field>
