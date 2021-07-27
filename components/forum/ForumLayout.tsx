8179d919b736b0f66fe5c55bae5d3eb6462998b0
tlylt <liu_yongliang@hotmail.com>
2021-07-27 19:54:56 +0800

    remove unuse import

--- components/forum/ForumLayout.tsx
+++ components/forum/ForumLayout.tsx
@@ -1,5 +1,4 @@
 import { Skeleton } from '@chakra-ui/react'
-import { Spinner } from '@chakra-ui/spinner'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
