84981b3dd5c90cb6b0c26b2c73304ab3689fec29
tlylt <liu_yongliang@hotmail.com>
2021-07-28 00:10:36 +0800

    Fix tests

--- cypress/integration/quiz/quiz_spec.ts
+++ cypress/integration/quiz/quiz_spec.ts
@@ -4,14 +4,14 @@ describe('Quiz page', function () {
     cy.visit('/quiz')
   })
   it('takes a quiz', function () {
-    cy.contains('Quiz')
+    cy.contains('Quiz', { timeout: 10000 })
 
     cy.get('[data-cy=quizTitle]').contains('Sample Quiz').click()
     cy.get('[data-cy=startQuizButton]').click()
 
     cy.get('[data-cy=seeQuizQuestions]', { timeout: 20000 }).click()
 
-    cy.contains('ans').click()
+    cy.contains('Answer').click()
     cy.contains('Submit').click()
     cy.contains('Try again?')
     cy.contains('Retry')
@@ -28,7 +28,7 @@ describe('Quiz page', function () {
 
   it('retries a quiz', function () {
     cy.contains('Retry').click()
-    cy.contains('wrong').click()
+    cy.contains('Wrong').click()
     cy.contains('Submit').click()
     cy.contains('Retry')
     cy.contains('Review')
